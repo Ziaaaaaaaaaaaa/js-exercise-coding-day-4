b31b0069f3a1c0dfdc87f1d3b55d11c5b7acdb53
Ziaaaaaaaaaaaa <ziakhan29042003gmail.com>
2023-05-19 10:25:09 +0200

    commit

---
+++ script/code.js
@@ -0,0 +1,39 @@
+// let person = [
+//     { id: 1, name: 'Zia Khan'}
+//     { id: 1, name: 'lana del rey'}
+// ];
+// console.log(person);
+
+// let people1 = ["zia" ,"lana","jv"];
+
+// let zia = people[0];
+// let lana = people[1]
+// console.log(people)
+// console.log(zia)
+// console.log(lana)
+
+// let numbers = [8,4,5,0]
+// numbers.sort();
+// console.log(numbers);
+
+let people = {
+    person1: "Zia",
+    person2: "Lana",
+    person3: "justin"
+}
+
+let people2 = {
+    person1: "aches",
+    person2: "shakes",
+    person3: "molly"
+}
+
+console.log(people);
+
+let arrayLog = [people, people2]
+
+console.log(arrayLog);
+
+
+
+
